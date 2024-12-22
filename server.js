eec5e598017673eafb80a86a58f86e0b65cbf33e
Jay sharma <jaysharma895@gmail.com>
2024-12-22 20:08:49 +0530

    Going to create my potfolio website

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello jay");
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.send("Hello jay");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server run on port"+process.env.PORT);
+})
